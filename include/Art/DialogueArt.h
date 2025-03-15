87bb69f5574529d2eb4e70b00b06045f8bac1a05
Cloudy <cloudisms@protonmail.com>
2025-03-15 18:21:22 -0400

    Remove unnecessary constant

--- include/Art/DialogueArt.h
+++ include/Art/DialogueArt.h
@@ -5,10 +5,10 @@ namespace art
 {
   namespace dialogue
   {
-    void roomEnemyMonologue(const bool ROOM_EXPLORED);
-    void roomLootMonologue(const bool ROOM_EXPLORED);
-    void roomMerchantMonologue(const bool ROOM_EXPLORED);
-    void roomExitMonologue(const bool ROOM_EXPLORED);
+    void roomEnemyMonologue(bool ROOM_EXPLORED);
+    void roomLootMonologue(bool ROOM_EXPLORED);
+    void roomMerchantMonologue(bool ROOM_EXPLORED);
+    void roomExitMonologue(bool ROOM_EXPLORED);
   }
 }
 
