98be69bca5a52dd4c6ef16c355c42cc20794146a
Cloudy <cloudisms@protonmail.com>
2025-03-16 01:26:42 -0400

    Cleanup Map

--- include/Map.h
+++ include/Map.h
@@ -3,9 +3,6 @@
 
 #include <string>
 
-using std::string;
-
-// Map Constants - feel free to change - have to both be odd numbers or positioning will be weird
 const int ROWS = 9;
 const int COLS = 9;
 
@@ -17,28 +14,31 @@ class Position
 public:
   char symbol;
   bool explored;
-  string contents;
-  Position() { symbol = ' '; explored = false; contents = " "; }
+  std::string contents;
+  Position() { symbol = ' '; explored = false; }
 };
 
 class Map
 {
-private:
-  Position position[ROWS][COLS];
-  int rowPos;
-  int colPos;
-
-  void generateMap();
-  void generateRooms(const string &ROOM_NAME, const char ROOM_SYMBOL, const int ROOM_DISTANCE, const int DUPE_DISTANCE, const int ROOMS_ALLOWED);
-  void mapPrint() const;
-  void mapMovement();
 public:
   Map();
   ~Map() { }
 
-  // Functions
+private:
+  // Helper Functions
+  void generateMap();
+  void generateRooms(const std::string &ROOM_NAME, const char ROOM_SYMBOL, const int ROOM_DISTANCE, const int DUPE_DISTANCE, const int ROOMS_ALLOWED);
+  void mapPrint() const;
+  void mapMovement();
+
+public:
   bool move();
-  string getRoomContents() const { return position[rowPos][colPos].contents; }
+  std::string getRoomContents() const { return position[rowPos][colPos].contents; }
+
+private:
+  Position position[ROWS][COLS];
+  int rowPos;
+  int colPos;
 };
 
 #endif // MAP_H
